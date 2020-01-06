b7e96901734c330682cab9e88198b1a00105998d
YusukZebs <simhoover9000@gmail.com>
2020-01-06 22:14:21 +0900

    Update test.py

--- Study - Test Area/test.py
+++ Study - Test Area/test.py
@@ -1,34 +1,28 @@
-import sys
-from PySide2.QtWidgets import QApplication, QWidget, QLabel
-from PySide2.QtGui import QIcon, QPixmap
+prime_list = set()
+value = 20
 
+for num in range(2, (value//2) + 1):
+    print(f"new loop; num is {num}")
+    if value % num == 0:
+        print(f"{num} is a factor of {value}." )
+        value = (value / num)
+        print(f"value changing to {value} because num after it aren't factors.")
+        if value == 1:
+            print(f"Adding {num} to prime_list")
+            prime_list.add(num)
+            break
+        print(f"Check prime in range {(num//2) + 2}")
+        for num2 in range(2, (num//2) + 2):
+            print(f"Checking if {num} can be divided by {num2}")
+            if num % num2 == 0 and num2 != num:
+                print(f"It can be divided and it's not {num}, so it's not a prime.")
+                break
+            else:
+                print(f"Adding {num} to prime_list")
+                prime_list.add(num)
+    else:
+        print(f"{num} is not a factor of {value}.")
 
-class App(QWidget):
-
-    def __init__(self):
-        super().__init__()
-        self.title = 'PySide2 image - pythonspot.com'
-        self.left = 100
-        self.top = 100
-        self.width = 640
-        self.height = 480
-        self.initUI()
-
-    def initUI(self):
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
-
-        # Create widget
-        label = QLabel(self)
-        pixmap = QPixmap(
-            "C:/Users/simho/OneDrive - konkuk.ac.kr/Python scripts/Study - Test Area/UI File/cat.jpg")
-        label.setPixmap(pixmap)
-        self.resize(pixmap.width(), pixmap.height())
-
-        self.show()
-
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = App()
-    sys.exit(app.exec_())
+prime_list = list(prime_list)
+prime_list.sort()
+print(prime_list[-1])
