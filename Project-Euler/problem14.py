b4a04956ffcc36213726438842b72510576dfce4
YusukZebs <simhoover9000@gmail.com>
2020-05-05 14:21:26 +0900

    random

--- Project-Euler/problem14.py
+++ Project-Euler/problem14.py
@@ -1,25 +1,39 @@
-n = 0
-highest_chain_starter = 0
+import matplotlib.pyplot as plt
+import numpy as np
 
-while n < 1_000_000:
-    collatz_list = []
-    n += 1
-    collatz_list.append(n)
-    if n % 2 == 0:
-        n = n/2
-    else:
-        n = 3*n + 1
+test_no = 1
+chain = []
+chain_len = 0
+interval_len = 0
 
-    collatz_list.append(n)
-    while n != 1:
-        if n % 2 == 0:
-            n = n/2
+pltpoint_x = []
+pltpoint_y = []
+
+while test_no < 10_000:
+    test_no += 1
+    val = test_no
+    pltpoint_x.append(test_no)
+    # print("test_no", test_no)
+
+    while val != 1:
+        if val%2 == 0:
+            val//=2
         else:
-            n = 3*n + 1
-        collatz_list.append(n)
+            val = 3*val + 1
+        chain.append(val)
+    print("this val's chain", len(chain))
+    pltpoint_y.append(len(chain))
+    if chain_len >= len(chain):
+        interval_len += 1
+    else:
+        chain_len = len(chain)
+        # print("Interval is ", interval_len)
+        interval_len = 0
+        chain.clear()
 
-    chain_len = len(collatz_list)
-    print(chain_len)
-    highest_chain_starter = n if chain_len > highest_chain_starter else highest_chain_starter
+fig, ax = plt.subplots()
+ax.plot(pltpoint_x, pltpoint_y)
 
-print(highest_chain_starter)
+print(chain_len)
+
+plt.show()
